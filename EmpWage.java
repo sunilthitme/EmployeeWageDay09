03bee25c823fb7ea7fd2b776f993f34257a76376
sunilthitme <thitamesunil808@gmail.com>
2021-12-16 20:04:26 +0530

    Part time

--- EmpWage.java
+++ EmpWage.java
@@ -6,7 +6,12 @@ public class EmpWage {
 		int dailyWage=0;
 		int empWagePerHour=20;
 		int workingPerHour=8;
-		int presentAbsent= (int)(Math.floor(Math.random() * 10)) % 2+1;
+		int partTimeHour=4;
+		int dayinMonth=20;
+		int totalWage=0;
+		
+		for(int i=0;i<=dayinMonth;i++) {
+		int presentAbsent= (int)(Math.floor(Math.random() * 10)) % 3+1;
 		switch (presentAbsent) {
 		case 1:
 			 System.out.print("Emplyee is Present");
@@ -14,13 +19,21 @@ public class EmpWage {
 			 
 			 break;
 		case 2:
-			 System.out.print("Emplyee is Absent");
+			 System.out.print("Employee is Present Part Time");
+			 dailyWage=partTimeHour*empWagePerHour;
+			 
+			 break; 
+		case 3:
+			 System.out.print("Employee is Absent");
+			 break;
 		}
-		 System.out.print(" Emplyee Daily Wage is "+dailyWage);
+		 System.out.println(" Employee Daily Wage is "+dailyWage);
+		  totalWage=totalWage+dailyWage;
 	}
-
+		 System.out.println(" Employee Monthly Wage is "+totalWage);
+	}
+        
 }
-
 	
 
 
